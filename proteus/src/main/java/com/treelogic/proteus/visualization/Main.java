91d04d4db6cea21e861aa9f9ae36b0fd027e8d09
Jorge Yagüe París <jorgeyp@gmail.com>
2016-04-14 11:27:13 +0200

    Add linechart

--- proteus/src/main/java/com/treelogic/proteus/visualization/Main.java
+++ proteus/src/main/java/com/treelogic/proteus/visualization/Main.java
@@ -1,8 +1,11 @@
 package com.treelogic.proteus.visualization;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.treelogic.proteus.visualization.model.Barchart;
+import com.treelogic.proteus.visualization.model.Linechart;
 import com.treelogic.proteus.visualization.model.Streamgraph;
 import com.treelogic.proteus.visualization.model.points.BasicPoint;
 import com.treelogic.proteus.visualization.model.points.StreamPoint;
@@ -30,8 +33,28 @@ public class Main {
 			.addPoint(new StreamPoint<Date, Integer, String>(new Date(),3,4, "B"))
 			.addPoint(new StreamPoint<Date, Integer, String>(new Date(),5,6, "C"));
 		
-		
+		//
+		//
+		// LINECHART
+		//
+		//
+		Linechart<Date, Integer> linechart = new Linechart<Date, Integer>();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/yy");
+		Date d1 = null, d2 = null, d3 = null;
+		try {
+			d1 = sdf.parse("01/16");
+			d2 = sdf.parse("02/16");
+			d3 = sdf.parse("03/16");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		linechart
+			.addPoint(new BasicPoint<Date, Integer>(d1, 1))
+			.addPoint(new BasicPoint<Date, Integer>(d2, 1))
+			.addPoint(new BasicPoint<Date, Integer>(d3, 1));
+
 		System.out.println(barchart.toJson());
 		System.out.println(streamgraph.toJson());
+		System.out.println(linechart.toJson());
 	}
 }
