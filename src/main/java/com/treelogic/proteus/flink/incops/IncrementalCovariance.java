6f618ceeb3d5dbe90abff6c6c1443abae1ca1ace
Ezequiel Cimadevilla Álvarez <zequi3000@hotmail.com>
2016-05-27 14:47:12 +0200

    Implemented IncrementalCovariance along with IncrementalCovarianceTest

---
+++ src/main/java/com/treelogic/proteus/flink/incops/IncrementalCovariance.java
@@ -0,0 +1,169 @@
+package com.treelogic.proteus.flink.incops;
+
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.functions.windowing.RichWindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.util.Collector;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IncrementalCovariance<IN>
+    extends RichWindowFunction<IN, Double, Tuple, GlobalWindow> {
+
+    private final static Long serialVersionUID = 1L;
+    private final String fieldX, fieldY;
+
+    /**
+     * Tuple4<xMean, yMean, Sum of deviation products, Element count>
+     */
+    private ValueStateDescriptor<Tuple4<MeanTuple, MeanTuple, Double, Double>> descriptor;
+
+    public IncrementalCovariance(String fieldX, String fieldY) {
+        if (fieldX == null || fieldX.equals("")
+            || fieldY == null || fieldY.equals("")) {
+            throw new IllegalArgumentException("Field cannot be empty");
+        }
+
+        this.fieldX = fieldX;
+        this.fieldY = fieldY;
+
+        descriptor = new ValueStateDescriptor<>(
+            "incremental-variance-descriptor",
+            TypeInformation.of(
+                new TypeHint<Tuple4<MeanTuple, MeanTuple, Double, Double>>() {}),
+            new Tuple4<>(new MeanTuple(), new MeanTuple(), 0d, 0d));
+    }
+
+    @Override
+    public void apply(Tuple tuple,
+                      GlobalWindow window,
+                      Iterable<IN> input,
+                      Collector<Double> out) throws Exception {
+
+        ValueState<Tuple4<MeanTuple, MeanTuple, Double, Double>> state =
+            getRuntimeContext().getState(descriptor);
+
+        // TODO Set initial size equals to window size?
+        List<Tuple2<Double, Double>> elems = new ArrayList<>();
+
+        for (IN in : input) {
+            Field fieldX = in.getClass().getDeclaredField(this.fieldX);
+            Field fieldY = in.getClass().getDeclaredField(this.fieldY);
+            fieldX.setAccessible(true);
+            fieldY.setAccessible(true);
+            elems.add(new Tuple2<>(
+                (Double) fieldX.get(in), (Double)fieldY.get(in)));
+        }
+
+        // Compute elems mean
+        double xMean2sum = 0, yMean2sum = 0, xMean2, yMean2;
+        for (Tuple2<Double, Double> t : elems) {
+            xMean2sum += t.f0;
+            yMean2sum += t.f1;
+        }
+
+        xMean2 = xMean2sum / elems.size();
+        yMean2 = yMean2sum / elems.size();
+
+        // currentSDP = current sum of deviation products
+        double currentSDP = 0;
+        for(Tuple2<Double, Double> t : elems) {
+            currentSDP += (t.f0 - xMean2) * (t.f1 - yMean2);
+        }
+
+        // Merge (if first iteration just save)
+        double elemsProcessed = state.value().f3;
+
+        if(elemsProcessed == 0) {
+            elemsProcessed = elems.size();
+            MeanTuple xmt = new MeanTuple(sumX(elems), elems.size());
+            MeanTuple ymt = new MeanTuple(sumY(elems), elems.size());
+            state.update(new Tuple4<>(xmt, ymt, currentSDP, (double) elems.size()));
+
+            // Naive covariance formula
+            out.collect(currentSDP / ( elems.size() - 1) );
+        } else {
+            double lastSDP = state.value().f2;
+            MeanTuple lastXMean = state.value().f0;
+            MeanTuple lastYMean = state.value().f1;
+            double xMeanDiff = xMean2 - lastXMean.mean();
+            double yMeanDiff = yMean2 - lastYMean.mean();
+            double sizeSum = elemsProcessed + elems.size();
+            double newSDP = lastSDP + currentSDP
+                + (elemsProcessed * elems.size() * xMeanDiff * yMeanDiff / sizeSum);
+
+            lastXMean.incNum(sumX(elems));
+            lastXMean.incDenom(elems.size());
+            lastYMean.incNum(sumY(elems));
+            lastYMean.incDenom(elems.size());
+
+            state.update(new Tuple4<>(
+                lastXMean, lastYMean, newSDP, elemsProcessed + elems.size()));
+
+            out.collect(newSDP / (sizeSum - 1));
+        }
+    }
+
+    private double sumX(List<Tuple2<Double, Double>> list) {
+        double sum = 0;
+        for(Tuple2<Double, Double> t : list) {
+            sum += t.f0;
+        }
+        return sum;
+    }
+
+    private double sumY(List<Tuple2<Double, Double>> list) {
+        double sum = 0;
+        for(Tuple2<Double, Double> t : list) {
+            sum += t.f1;
+        }
+        return sum;
+    }
+
+    private static class MeanTuple {
+        private double num , denom;
+
+        public MeanTuple() { }
+
+        public MeanTuple(double num, double denom) {
+            this.num = num;
+            this.denom = denom;
+        }
+
+        public double getNum() {
+            return num;
+        }
+
+        public void setNum(double num) {
+            this.num = num;
+        }
+
+        public double getDenom() {
+            return denom;
+        }
+
+        public void setDenom(double denom) {
+            this.denom = denom;
+        }
+
+        public double mean() {
+            return num / denom;
+        }
+
+        public void incNum(double value) {
+            setNum(getNum() + value);
+        }
+
+        public void incDenom(double value) {
+            setDenom(getDenom() + value);
+        }
+    }
+}
---
+++ src/test/java/com/treelogic/proteus/incOperators/IncrementalCovarianceTest.java
@@ -0,0 +1,87 @@
+package com.treelogic.proteus.incOperators;
+
+import com.treelogic.proteus.flink.examples.airquality.AirRegister;
+import com.treelogic.proteus.flink.incops.IncrementalCovariance;
+import com.treelogic.proteus.flink.incops.IncrementalVariance;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.flinkspector.core.collection.ExpectedRecords;
+import org.flinkspector.datastream.DataStreamTestBase;
+import org.flinkspector.datastream.DataStreamTestEnvironment;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class IncrementalCovarianceTest extends DataStreamTestBase {
+
+    private static DataStreamTestEnvironment env = null;
+
+    @BeforeClass
+    public static void beforeClass() {
+        try {
+            env = DataStreamTestEnvironment.createTestEnvironment(1);
+            env.setTimeoutInterval(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void naiveCovarianceTest() {
+        DataStream<Double> stream = this
+            .createTestStream(createDataset(7))
+            .keyBy("station")
+            .countWindow(7)
+            .apply(new IncrementalCovariance<AirRegister>("o3", "co"));
+
+        ExpectedRecords<Double> expected =
+            new ExpectedRecords<Double>().expectAll(asList(-340.3333333333333d));
+
+        assertStream(stream, expected);
+    }
+
+    @Test
+    public void combinedCovarianceTest() {
+        DataStream<Double> stream = this
+            .createTestStream(createDataset(14))
+            .keyBy("station")
+            .countWindow(7)
+            .apply(new IncrementalCovariance<AirRegister>("o3", "co"));
+
+        ExpectedRecords<Double> expected =
+            new ExpectedRecords<Double>().expectAll(asList(
+                -340.3333333333333d,
+                -322.4945054945055d));
+
+        assertStream(stream, expected);
+    }
+
+    private List<AirRegister> createDataset(int datasetSize) {
+        String station = "station";
+
+        List<Double> o3registers = asList(
+            4d, 8d, 41d, 11d, 9d, 87d, 23d,
+            43d, 12d, 9d, 98d, 23d, 28d, 65d);
+
+        List<Double> coRegisters = asList(
+            15d, 1d, 22d, 31d, 90d, 0d, 2d,
+            87d, 26d, 61d, 1d, 9d, 33d, 8d);
+
+        // Return dataset
+        List<AirRegister> dataset = new ArrayList<>(datasetSize);
+
+        for(int i = 0; i < datasetSize; i++) {
+            Double o3 = o3registers.get(i);
+            Double co = coRegisters.get(i);
+            AirRegister ar = new AirRegister();
+            ar.setStation(station);
+            ar.setO3(o3);
+            ar.setCo(co);
+            dataset.add(ar);
+        }
+        return dataset;
+    }
+}
