0eb3bf897e19585a59d6a24d6954a5f009f668b4
Nacho García Fernández <nachogarcia91@gmail.com>
2016-07-05 12:15:43 +0200

    Update javadocs-to-gh-pages.sh

--- javadocs-to-gh-pages.sh
+++ javadocs-to-gh-pages.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+JAVADOC_DIR=$(pwd)
+
+#Create javadocs
+mvn javadoc:javadoc
+
 echo -e "Publishing javadoc...\n"
 
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+echo -e "Cloning the gh-pages branch...\n"
 
+
+git clone --quiet --branch=gh-pages https://github.com/proteus-h2020/proteus-backend gh-pages > /dev/null
+cd gh-pages
+rm -rf ./*
+cp -R $JAVADOC_DIR/target/site/apidocs/* .
+git add -f .
+git commit -m "[DOCS-$TRAVIS_BUILD_NUMBER] Add javadocs"
+git push -fq origin gh-pages > /dev/null
 
 echo -e "Published Javadoc to gh-pages.\n"
   
