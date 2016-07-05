24c520a4fb9b46d90596af9dfe578378e0caf1a8
Nacho García Fernández <nachogarcia91@gmail.com>
2016-07-05 13:06:38 +0200

    Update javadocs-to-gh-pages.sh

--- javadocs-to-gh-pages.sh
+++ javadocs-to-gh-pages.sh
@@ -14,9 +14,11 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 echo -e "Cloning the gh-pages branch...\n"
 
-git remote add upstream "https://0xNacho:$GH_TOKEN@github.com/proteus-h2020/proteus-backend.git"
-git fetch upstream
-git reset upstream/gh-pages
+git clone "https://0xNacho:$GH_TOKEN@github.com/proteus-h2020/proteus-backend.git" --branch=gh-pages gh-pages
+cd gh-pages
+git remote rm origin
+git remote add origin "https://0xNacho:$GH_TOKEN@github.com/proteus-h2020/proteus-backend.git"
+
 rm -rf ./*
 cp -R $JAVADOC_DIR/target/site/apidocs/* .
 git add -A .
