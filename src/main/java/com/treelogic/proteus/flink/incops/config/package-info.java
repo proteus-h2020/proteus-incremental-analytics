de5c4b3dd376ecd7eb42f12b3bd1715399c08641
0xNacho <nachogarcia91@gmail.com>
2016-07-11 14:33:49 +0200

    Add some docs

---
+++ src/main/java/com/treelogic/proteus/flink/incops/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * It contains a set of configuration classes which make able to adjust every user parameter.
+ */
+package com.treelogic.proteus.flink.incops.config;
---
+++ src/main/java/com/treelogic/proteus/flink/sink/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * It contains a WebsocketSink utilized to real-time send all the processed data to a web browser.
+ */
+package com.treelogic.proteus.flink.sink;
